9b1292e7c83024b02ad1919c00c7be56112bb351
Shoubhit Dash <shoubhit2005@gmail.com>
2023-11-07 19:43:52 +0530

    make eslint ignore tailwind config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,7 +13,7 @@ module.exports = {
 	parserOptions: {
 		ecmaVersion: "latest",
 		sourceType: "module",
-		project: ["./tsconfig.json", "./tsconfig.node.json"],
+		// project: ["./tsconfig.json", "./tsconfig.node.json"],
 		tsconfigRootDir: __dirname,
 	},
 	plugins: ["react-refresh"],
