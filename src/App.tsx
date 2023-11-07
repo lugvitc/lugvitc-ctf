c797ae214f506e21c72592b1f8d57e7e589d00c6
Shoubhit Dash <shoubhit2005@gmail.com>
2023-11-07 19:50:38 +0530

    format

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
 	return (
-		<main className="flex flex-col items-center justify-center h-screen">
-			<h1 className="font-semibold text-4xl">CTF</h1>
+		<main className="flex h-screen flex-col items-center justify-center">
+			<h1 className="text-4xl font-semibold">CTF</h1>
 		</main>
 	);
 }
