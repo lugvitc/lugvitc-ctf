0a52fcee00a0bd48ca727bc86a08c3292f92f714
WizzyGeek <ojasscoding@gmail.com>
2023-12-23 21:44:03 +0530

    chore: format

--- src/components/LeftSideBar.tsx
+++ src/components/LeftSideBar.tsx
@@ -2,72 +2,74 @@ import { NavLink, useLocation } from "react-router-dom";
 import { leftSidebarLinks } from "../constants";
 
 interface Location {
-    pathname: string;
+	pathname: string;
 }
 
 const isAuthenticated: boolean = false;
 
 const LeftSideBar = () => {
-    const location = useLocation() as Location;
-    const pathname = location.pathname;
+	const location = useLocation() as Location;
+	const pathname = location.pathname;
 
-    return (
-        <div className="fixed left-0 top-0 hidden h-screen flex-col justify-between overflow-y-auto bg-black p-6 pt-36 max-lg:flex max-lg:w-[200px] max-sm:hidden">
-            <div className="flex flex-1 flex-col gap-6">
-                {leftSidebarLinks.map((link) => {
-                    const isActive = pathname === link.route;
-                    return (
-                        <NavLink
-                            key={link.label}
-                            to={link.route}
-                            className={`${isActive
-                                    ? "primary-gradient text-light-900 rounded-lg"
-                                    : "text-dark300_light900"
-                                }  flex justify-start gap-4 rounded-xl bg-transparent p-4`}
-                        >
-                            <img
-                                src={link.img}
-                                alt={link.label}
-                                width={20}
-                                height={20}
-                                className={`${isActive ? "" : "invert-colors"}`}
-                            />
-                            <p
-                                className={`${isActive
-                                        ? "text-[15px] font-bold leading-[140%]"
-                                        : "text-[18px] font-medium leading-[25.2px]"
-                                    }`}
-                            >
-                                {link.label}
-                            </p>
-                        </NavLink>
-                    );
-                })}
-            </div>
-            {/* TODO: ADD REAL AUTH */}
+	return (
+		<div className="fixed left-0 top-0 hidden h-screen flex-col justify-between overflow-y-auto bg-black p-6 pt-36 max-lg:flex max-lg:w-[200px] max-sm:hidden">
+			<div className="flex flex-1 flex-col gap-6">
+				{leftSidebarLinks.map((link) => {
+					const isActive = pathname === link.route;
+					return (
+						<NavLink
+							key={link.label}
+							to={link.route}
+							className={`${
+								isActive
+									? "primary-gradient text-light-900 rounded-lg"
+									: "text-dark300_light900"
+							}  flex justify-start gap-4 rounded-xl bg-transparent p-4`}
+						>
+							<img
+								src={link.img}
+								alt={link.label}
+								width={20}
+								height={20}
+								className={`${isActive ? "" : "invert-colors"}`}
+							/>
+							<p
+								className={`${
+									isActive
+										? "text-[15px] font-bold leading-[140%]"
+										: "text-[18px] font-medium leading-[25.2px]"
+								}`}
+							>
+								{link.label}
+							</p>
+						</NavLink>
+					);
+				})}
+			</div>
+			{/* TODO: ADD REAL AUTH */}
 
-            {isAuthenticated === false && (
-                <div className="flex flex-col gap-6">
-                    <NavLink to="/log-in">
-                        <button className="w-full rounded-xl border px-4 py-2 text-[15px] font-bold leading-[140%] shadow-none hover:border-[#78CBFF] hover:text-[#78CBFF]">
-                            Log In
-                        </button>
-                    </NavLink>
-                    <NavLink to="/sign-up">
-                        <button className="w-full rounded-xl border border-[#78CBFF] px-4 py-2 text-[15px] font-bold leading-[140%] text-[#78CBFF] shadow-none hover:border-white hover:text-white">
-                            Sign Up
-                        </button>
-                    </NavLink>
-                </div>
-            )}
-            <NavLink
-                to={`/more`}
-                className={` mb-6 mt-6 flex justify-center rounded-xl border p-2 px-3`}
-            >
-                MORE
-            </NavLink>
-        </div>
-    );
+			{isAuthenticated === false && (
+				<div className="flex flex-col gap-6">
+					<NavLink to="/log-in">
+						<button className="w-full rounded-xl border px-4 py-2 text-[15px] font-bold leading-[140%] shadow-none hover:border-[#78CBFF] hover:text-[#78CBFF]">
+							Log In
+						</button>
+					</NavLink>
+					<NavLink to="/sign-up">
+						<button className="w-full rounded-xl border border-[#78CBFF] px-4 py-2 text-[15px] font-bold leading-[140%] text-[#78CBFF] shadow-none hover:border-white hover:text-white">
+							Sign Up
+						</button>
+					</NavLink>
+				</div>
+			)}
+			<NavLink
+				to={`/more`}
+				className={` mb-6 mt-6 flex justify-center rounded-xl border p-2 px-3`}
+			>
+				MORE
+			</NavLink>
+		</div>
+	);
 };
 
 export default LeftSideBar;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,103 +5,105 @@ import { homeNavbarLinks } from "../../constants";
 import MobileNavbar from "../MobileNavbar";
 
 interface Location {
-    pathname: string;
+	pathname: string;
 }
 const Navbar = () => {
-    const [isOpen, setIsOpen] = useState<boolean>(false);
-    const [isMoreActive, setIsMoreActive] = useState(false);
-    const location = useLocation() as Location;
-    const pathname = location.pathname;
-    const isAuthenticated = false; // TODO: Replace with actual auth state
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const [isMoreActive, setIsMoreActive] = useState(false);
+	const location = useLocation() as Location;
+	const pathname = location.pathname;
+	const isAuthenticated = false; // TODO: Replace with actual auth state
 
-    return (
-        <nav className="shadow-light-300 fixed z-50 flex max-h-[140px] w-full items-center justify-between p-6 dark:shadow-none sm:px-12">
-            <div className="flex items-center justify-center gap-4">
-                <img
-                    src="src/assets/images/club-logo.png"
-                    alt="logo"
-                    className="h-[52px] w-[52px]"
-                />
-                <span className="font-DM-Mono text-white max-lg:hidden">
-                    Linux Club VITC
-                </span>
-            </div>
-            <div className="flex items-center justify-center gap-6 max-lg:hidden">
-                <div className="flex items-center justify-center gap-4">
-                    <div className="flex items-center justify-center gap-4">
-                        {homeNavbarLinks.map((item: HomeNavbarLinks) => {
-                            const isActive = pathname === item.route;
-                            return (
-                                <NavLink
-                                    key={item.label}
-                                    to={item.route}
-                                    className={`${isActive
-                                            ? "primary-gradient rounded-xl text-[15px] font-bold leading-[140%]"
-                                            : ""
-                                        } flex w-[80px] justify-center px-4 py-2 font-DM-Mono outline-none`}
-                                    onClick={() => {
-                                        if (isMoreActive === true) {
-                                            setIsMoreActive(false);
-                                        }
-                                    }}
-                                >
-                                    {item.label}
-                                </NavLink>
-                            );
-                        })}
-                    </div>
+	return (
+		<nav className="shadow-light-300 fixed z-50 flex max-h-[140px] w-full items-center justify-between p-6 dark:shadow-none sm:px-12">
+			<div className="flex items-center justify-center gap-4">
+				<img
+					src="src/assets/images/club-logo.png"
+					alt="logo"
+					className="h-[52px] w-[52px]"
+				/>
+				<span className="font-DM-Mono text-white max-lg:hidden">
+					Linux Club VITC
+				</span>
+			</div>
+			<div className="flex items-center justify-center gap-6 max-lg:hidden">
+				<div className="flex items-center justify-center gap-4">
+					<div className="flex items-center justify-center gap-4">
+						{homeNavbarLinks.map((item: HomeNavbarLinks) => {
+							const isActive = pathname === item.route;
+							return (
+								<NavLink
+									key={item.label}
+									to={item.route}
+									className={`${
+										isActive
+											? "primary-gradient rounded-xl text-[15px] font-bold leading-[140%]"
+											: ""
+									} flex w-[80px] justify-center px-4 py-2 font-DM-Mono outline-none`}
+									onClick={() => {
+										if (isMoreActive === true) {
+											setIsMoreActive(false);
+										}
+									}}
+								>
+									{item.label}
+								</NavLink>
+							);
+						})}
+					</div>
 
-                    {/* TODO: Replace with actual auth state */}
+					{/* TODO: Replace with actual auth state */}
 
-                    {!isAuthenticated && (
-                        <>
-                            <NavLink to="/log-in">
-                                <button className="w-full rounded-xl border px-4 py-2 text-[15px] font-bold leading-[140%] shadow-none hover:border-[#78CBFF] hover:text-[#78CBFF]">
-                                    Log In
-                                </button>
-                            </NavLink>
-                            <NavLink to="/sign-up">
-                                <button className="w-full rounded-xl border border-[#78CBFF] px-4 py-2 text-[15px] font-bold leading-[140%] text-[#78CBFF] shadow-none hover:border-white hover:text-white">
-                                    Sign Up
-                                </button>
-                            </NavLink>
-                        </>
-                    )}
-                </div>
-                <NavLink
-                    to={`/more`}
-                    className={`${isMoreActive ? "bg-[#002133]" : ""
-                        } rounded-xl border p-2 px-3`}
-                    onClick={() => {
-                        setIsMoreActive(!isMoreActive);
-                    }}
-                >
-                    MORE
-                </NavLink>
-            </div>
-            {isOpen ? (
-                <img
-                    src="src/assets/icons/close.svg"
-                    alt="extend"
-                    className="hidden h-[45px] w-[45px] cursor-pointer invert max-sm:flex"
-                    onClick={() => {
-                        setIsOpen(!isOpen);
-                    }}
-                />
-            ) : (
-                <img
-                    src="src/assets/icons/hamburger.png"
-                    alt="extend"
-                    className="hidden h-[45px] w-[45px] cursor-pointer invert max-sm:flex"
-                    onClick={() => {
-                        setIsOpen(!isOpen);
-                    }}
-                />
-            )}
+					{!isAuthenticated && (
+						<>
+							<NavLink to="/log-in">
+								<button className="w-full rounded-xl border px-4 py-2 text-[15px] font-bold leading-[140%] shadow-none hover:border-[#78CBFF] hover:text-[#78CBFF]">
+									Log In
+								</button>
+							</NavLink>
+							<NavLink to="/sign-up">
+								<button className="w-full rounded-xl border border-[#78CBFF] px-4 py-2 text-[15px] font-bold leading-[140%] text-[#78CBFF] shadow-none hover:border-white hover:text-white">
+									Sign Up
+								</button>
+							</NavLink>
+						</>
+					)}
+				</div>
+				<NavLink
+					to={`/more`}
+					className={`${
+						isMoreActive ? "bg-[#002133]" : ""
+					} rounded-xl border p-2 px-3`}
+					onClick={() => {
+						setIsMoreActive(!isMoreActive);
+					}}
+				>
+					MORE
+				</NavLink>
+			</div>
+			{isOpen ? (
+				<img
+					src="src/assets/icons/close.svg"
+					alt="extend"
+					className="hidden h-[45px] w-[45px] cursor-pointer invert max-sm:flex"
+					onClick={() => {
+						setIsOpen(!isOpen);
+					}}
+				/>
+			) : (
+				<img
+					src="src/assets/icons/hamburger.png"
+					alt="extend"
+					className="hidden h-[45px] w-[45px] cursor-pointer invert max-sm:flex"
+					onClick={() => {
+						setIsOpen(!isOpen);
+					}}
+				/>
+			)}
 
-            <MobileNavbar isOpen={isOpen} setIsOpen={setIsOpen} />
-        </nav>
-    );
+			<MobileNavbar isOpen={isOpen} setIsOpen={setIsOpen} />
+		</nav>
+	);
 };
 
 export default Navbar;
