2894315c2005b8d02d15a0cb04307062ed94fd8d
Shreyas <shreyas.mohanty03@gmail.com>
2023-12-27 18:38:36 +0530

    color object added to extend tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,11 @@ module.exports = {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 
 	theme: {
-		colors: {
-			'sky-blue': '#78CBFF',
-			'midnight-blue': '#002133',
-		},
 		extend: {
+			colors: {
+				'sky-blue': '#78CBFF',
+				'midnight-blue': '#002133',
+			},
 			fontFamily: {
 				'source-code-pro': ['Source Code Pro', 'monospace'],
 				'DM-Mono': ['DM Mono', 'monospace']
