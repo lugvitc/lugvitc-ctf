694a19bbde53428565bb7439551b95b5d64ee9c7
sanket9673 <sanketch9673@gmail.com>
2023-12-30 16:30:17 +0530

    Add: Login Page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -8,9 +8,7 @@ export default function Login() {
 
 				<form action="#" method="POST">
 					<div className="mb-4">
-						<label htmlFor="username"className="mb-2 block text-sm font-medium text-black">
-							Team Name
-						</label>
+						<label htmlFor="username"className="mb-2 block text-sm font-medium text-black">Team Name</label>
 						<input
 							type="text"
 							id="username"
