016f17d6feed853311fc20ffc4f3c487ac2a965f
theditor <theditormcpe@gmail.com>
2024-01-10 00:50:36 +0530

    question view 1

--- src/components/preevent/Question.tsx
+++ src/components/preevent/Question.tsx
@@ -2,21 +2,37 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export interface QuestionData {
-    question: string;
-    points: number;
+	title: string;
+	description: string;
+	points: number;
 }
 
 export default function Question({ date }: { date: number }) {
-    const [question, setQuestion] = useState<QuestionData | null>(null);
-    
-    useEffect(() => {
-        axios.get(`/ctf/question/${date}`).then((res: { data: QuestionData }) => {
-            setQuestion(res.data);
-        }).catch(() => {});
-    });
+	// set default value as null for production
+	const [question, setQuestion] = useState<QuestionData | null>({
+		title: "Reuben",
+		description: "in the memory of reuben - the pig",
+		points: 500,
+	});
 
-    if (!question)
-        return (<>Loading...</>);
+	useEffect(() => {
+		axios
+			.get(`/ctf/question/${date}`)
+			.then((res: { data: QuestionData }) => {
+				setQuestion(res.data);
+			})
+			.catch(() => {});
+	});
 
-    return (<></>);
+	if (!question) return <>Loading...</>;
+
+	return (
+		<div className="top-0 m-2 flex h-full w-full flex-col rounded-xl border border-gray-700 bg-slate-900 shadow-sm shadow-slate-700/[.7]">
+			<div className="p-4">
+				<h3 className="text-lg font-bold text-white">{question.title}</h3>
+				<p className="mt-2 text-gray-300">{question.description}</p>
+				<p className="mt-2 text-gray-500">{question.points}</p>
+			</div>
+		</div>
+	);
 }
