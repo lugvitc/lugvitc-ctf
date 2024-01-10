7642a3dbb9d772e11e31d3329b131d4cd771a9df
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-01-10 21:36:27 +0530

    ✨ Popup Added

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 import clubLogo from "../assets/images/club-logo.png";
+// import axios from "axios";
 
 import { useState } from "react";
 
@@ -8,24 +9,43 @@ const SignUp = () => {
 	// DUMMY AUTHENTICATION
 	const isAuthenticated = false;
 
-	const [name, setName] = useState("");
-	const [password, setPassword] = useState("");
-	const [members, setMembers] = useState<string[]>([]);
+	const [name, setName] = useState<string>("");
+	const [password, setPassword] = useState<string>("");
+	const [members, setMembers] = useState<string[]>(["", "", ""]);
+	const [msg, setMsg] = useState({ msg: "", color: "" });
+	const [showPopUp, setShowPopUp] = useState(false);
+	const [submitted, setSubmitted] = useState(false);
+
+	const msgCode = (status: number, msg_code: string) => {
+		if (status >= 200 && status < 400) {
+			setMsg({ msg: msg_code, color: "green" });
+		} else if (status >= 400 && status < 500) {
+			setMsg({ msg: msg_code, color: "red" });
+		}
+		console.log(msg);
+	};
 
 	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		console.log("Name:", name);
-		console.log("Pass:", password);
-		console.log(members);
+		setSubmitted(true);
+		// await axios.post("url/signup", {
+		// 	'name': name,
+		// 	'password': password,
+		// 	'tags' : members
+		// }).then((res)=>{
+		// 	msgCode(res.status, res.data.msg_code)
+		// })
+		setTimeout(() => {
+			console.log("hi");
+			setShowPopUp(true);
+			setSubmitted(false);
+		}, 10000);
+		msgCode(200, "Team Created");
 		setName("");
 		setPassword("");
 		setMembers(() => {
-			for (let i = 0; i < members.length; i++) {
-				members.pop();
-			}
-			return members;
+			return ["", "", ""];
 		});
-		console.log(members);
 	};
 
 	const handleInputChange = (index: number, value: string) => {
@@ -34,6 +54,11 @@ const SignUp = () => {
 		setMembers(updatedTeamMembers);
 	};
 
+	const handlePopup = () => {
+		setShowPopUp(false);
+	};
+
+	console.log(submitted);
 	return (
 		<>
 			{isAuthenticated && <Navigate to="/sign-in"></Navigate>}
@@ -61,6 +86,7 @@ const SignUp = () => {
 					</div>
 					<div className="basis-3/4 md:w-96">
 						<form
+							id="sign-up-form"
 							className="flex flex-col items-center justify-center gap-2 p-6 text-base md:gap-4 md:p-10 md:text-lg"
 							onSubmit={handleSubmit}
 						>
@@ -112,20 +138,49 @@ const SignUp = () => {
 									onChange={(e) => handleInputChange(2, e.target.value)}
 								/>
 							</label>
-							<button
-								type="submit"
-								className="align-center w-full rounded-xl bg-midnight-blue p-1 font-normal md:p-2"
-							>
-								Sign Up
-							</button>
+							<div className="flex h-12 w-full justify-center">
+								{submitted ? (
+									<div className="flex flex-row gap-2">
+										<div className="h-4 w-4 animate-bounce rounded-full bg-blue-700 [animation-delay:.7s]"></div>
+										<div className="h-4 w-4 animate-bounce rounded-full bg-blue-700 [animation-delay:.3s]"></div>
+										<div className="h-4 w-4 animate-bounce rounded-full bg-blue-700 [animation-delay:.7s]"></div>
+									</div>
+								) : (
+									<button
+										type="submit"
+										className="align-center w-full rounded-xl bg-midnight-blue p-1 font-normal md:p-2"
+									>
+										Sign In
+									</button>
+								)}
+							</div>
 						</form>
 					</div>
 				</div>
 				<div className="text-xs md:text-lg">
-					Already Logged In? Click{" "}
+					Already Logged In?{" "}
 					<NavLink to="/log-in" className="text-blue-500">
-						Here
+						Log In
 					</NavLink>
+				</div>
+				<div
+					className={`absolute top-0 z-10 flex h-32 w-52 origin-top flex-col items-center justify-center gap-1 rounded-3xl bg-white p-2 font-source-code-pro duration-200 md:w-96 md:p-2 ${
+						!showPopUp
+							? "scale-y-0"
+							: msg["color"] === "green"
+								? "scale-y-150 border-8 border-green-600 text-green-600"
+								: "scale-y-150 border-8 border-red-600 text-red-600"
+					}`}
+				>
+					<span className="flex basis-3/4 items-center text-base font-semibold md:text-xl">
+						{msg["msg"]}
+					</span>
+					<button
+						onClick={handlePopup}
+						className="duration-400 relative flex items-center self-end overflow-hidden rounded-[50px] bg-black px-8 py-1 text-xs font-bold uppercase text-white shadow-md transition-all ease-in-out before:absolute before:-left-full before:top-0 before:z-[-1] before:h-full before:w-full before:rounded-[50px] before:bg-gradient-to-r before:from-blue-500 before:to-blue-300 before:transition-all before:duration-500 before:ease-in-out hover:scale-105 hover:text-white hover:shadow-lg hover:before:left-0 active:scale-90 md:text-sm"
+					>
+						CLOSE
+					</button>
 				</div>
 			</div>
 		</>
