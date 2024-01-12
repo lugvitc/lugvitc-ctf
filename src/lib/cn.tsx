ef7914be37055c4071d5cb3cfae6b6606e76b1df
Shoubhit Dash <shoubhit2005@gmail.com>
2024-01-12 14:20:28 +0530

    Format

--- src/lib/cn.tsx
+++ src/lib/cn.tsx
@@ -1,6 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
- 
+
 export default function cn(...args: ClassValue[]): string {
-    return twMerge(clsx(...args));
+	return twMerge(clsx(...args));
 }
