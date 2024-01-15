82569d18851eb417ed1b1b12a7effa0053f1b564
Shreyas <shreyas.mohanty03@gmail.com>
2024-01-15 22:25:09 +0530

    conditional routing accorind to preevent

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,15 +1,23 @@
 import { Navigate, Outlet } from "react-router-dom";
+import Navbar from "../components/shared/Navbar";
+import AuthProvider, { useUserContext } from "../context/AuthContext";
 
 const AuthLayout = () => {
-	const isAuthenticated = false;
+	const { isAuthenticated } = useUserContext();
+	console.log(isAuthenticated);
 	return (
 		<>
 			{isAuthenticated ? (
 				<Navigate to="/" />
 			) : (
 				<>
+					<AuthProvider>
+						<Navbar />
+					</AuthProvider>
 					<section className="flex flex-1 flex-col items-center justify-center py-10">
-						<Outlet />
+						<AuthProvider>
+							<Outlet />
+						</AuthProvider>
 					</section>
 				</>
 			)}
