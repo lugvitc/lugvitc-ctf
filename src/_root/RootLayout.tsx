d7b0239bd0ac7c1894e6355d19706aa96e223406
Shreyas <shreyas.mohanty03@gmail.com>
2024-01-15 22:25:33 +0530

    conditional routing updated

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,16 +1,28 @@
 import { Outlet } from "react-router-dom";
-// import { Navigate } from 'react-router-dom';
 import Navbar from "../components/shared/Navbar";
+import AuthProvider from "../context/AuthContext";
 
 const RootLayout = () => {
-	// const isAuthenticated = false;
+	const currentDate = new Date();
+	const targetDate = new Date("2024-01-29");
+
+	const renderAuthProviderAndNavbar = () => {
+		if (currentDate > targetDate) {
+			return (
+				<AuthProvider>
+					<Navbar />
+				</AuthProvider>
+			);
+		}
+		return null;
+	};
 
 	return (
 		<>
 			<div className="w-full md:flex">
-				<Navbar />
+				{renderAuthProviderAndNavbar()}
 
-				<section className="flex w-full items-center justify-center ">
+				<section className="flex w-full items-center justify-center">
 					<Outlet />
 				</section>
 			</div>
