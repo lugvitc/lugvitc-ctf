216e21996cb594e7007ab65cc163e9ab14d68c30
Shreyas <shreyas.mohanty03@gmail.com>
2024-01-15 22:25:21 +0530

    forms page updated

--- src/_auth/forms/LoginPage.tsx
+++ src/_auth/forms/LoginPage.tsx
@@ -1,9 +1,12 @@
 import { useNavigate, NavLink, Navigate } from "react-router-dom";
 import clubLogo from "../../assets/images/club-logo.png";
+import eventLogo from "../../assets/images/passwordctf-logo.png";
 import axios from "axios";
 
 import { useState } from "react";
 import { useUserContext } from "../../context/AuthContext";
+import { URL_ORIGIN } from "../../constants";
+import Background from "../../components/shared/Background";
 
 interface LoginResponse {
 	msg_code: string;
@@ -24,8 +27,12 @@ const LoginPage = () => {
 	const msgCode = (status: number, msg_code: string) => {
 		if (status >= 200 && status < 400) {
 			setMsg({ msg: msg_code, color: "green" });
+			handlePopup();
+			setShowPopUp(true);
 		} else if (status >= 400 && status < 500) {
 			setMsg({ msg: msg_code, color: "red" });
+			handlePopup();
+			setShowPopUp(true);
 		}
 		console.log(msg);
 	};
@@ -38,12 +45,12 @@ const LoginPage = () => {
 
 		// localStorage.setItem("jwt_token", "response.data.access_token");
 		// setIsAuthenticated(true);
-		// navigate("/leaderboard");
+		// navigate("/play");
 
 		// AUTHENTICATION
 
 		axios
-			.post<LoginResponse>("http://localhost:5000/api/auth/login", {
+			.post<LoginResponse>(`${URL_ORIGIN}/auth/login`, {
 				name: name,
 				password: password,
 			})
@@ -76,36 +83,52 @@ const LoginPage = () => {
 	};
 
 	const handlePopup = () => {
-		setShowPopUp(false);
+		setShowPopUp(true);
+		setTimeout(() => {
+			setShowPopUp(false);
+			setSubmitted(false);
+		}, 1800);
 	};
+
 	return (
 		<>
 			{isAuthenticated && <Navigate to="/"></Navigate>}
-			<div className="flex h-screen flex-col items-center justify-center gap-2 overflow-auto p-20 font-source-code-pro">
-				<div className="box flex flex-col justify-center rounded-lg bg-black p-6 md:p-10">
-					<div className="flex w-full basis-1/4 flex-col gap-4 md:w-96 md:gap-6">
+			<Background />
+			<div className="left-1/8 absolute top-[69px] z-10 flex h-[900px] w-[750px] flex-col items-center justify-center gap-2 overflow-auto p-20 font-source-code-pro">
+				<div className=" flex h-full w-full flex-col items-center justify-center rounded-lg border border-animation-green bg-black p-6 shadow-md shadow-animation-green md:p-10">
+					<div className="flex w-full basis-1/4 flex-col items-center justify-center gap-4 md:w-96 md:gap-6">
 						<div className="flex justify-center">
 							<div
-								className={`flex h-12 w-48 items-center justify-center text-lg font-bold md:h-16 md:w-60 md:text-xl`}
+								className={`flex h-12 w-48 items-center justify-center gap-4 text-lg font-bold md:h-16 md:w-60 md:text-xl`}
 							>
 								<img
 									src={clubLogo}
-									alt=""
-									className="h-full w-full object-contain"
+									alt="logo"
+									className="h-16 w-16 object-contain"
 								/>
-								<span>PasswordCTF</span>
+								<h2
+									className="hero glitch layers font-source-code-pro"
+									data-text="Linux Club"
+								>
+									<span className=" font-source-code-pro">PasswordCTF</span>
+								</h2>
+								<img
+									src={eventLogo}
+									alt="event-logo"
+									className="h-16 w-16 object-contain"
+								/>
 							</div>
 						</div>
-						<div className="text-center text-base font-semibold md:text-lg">
-							Welcome back
+						<div className="text-center text-[25px] font-bold  text-animation-green">
+							Welcome Hacker
 						</div>
-						<div className="text-center text-sm  text-gray36 ">
-							To solve ctf, please enter your details
+						<div className="mt-11 text-center  text-sm text-gray-300">
+							Engage the warp drive and set course for the flag.
 						</div>
 					</div>
-					<div className="basis-3/4 md:w-96">
+					<div className="w-96 basis-3/4">
 						<form
-							className="flex flex-col items-center justify-center gap-2 p-6 text-base md:gap-4 md:p-10 md:text-lg"
+							className="flex w-full flex-col items-center justify-center gap-2 p-6 text-base md:gap-4 md:p-10 md:text-lg"
 							onSubmit={(e) => {
 								e.preventDefault();
 								handleSubmit(e);
@@ -114,7 +137,7 @@ const LoginPage = () => {
 							<label className="w-full">
 								Team Name
 								<input
-									className="my-2 block w-full rounded-lg bg-dark-grayish-blue md:my-4"
+									className="my-2 block h-[46px] w-80 rounded-lg bg-dark-grayish-blue px-4 py-2 md:my-4"
 									type="text"
 									value={name}
 									onChange={(e) => setName(e.target.value)}
@@ -124,24 +147,24 @@ const LoginPage = () => {
 							<label className="w-full">
 								Password
 								<input
-									className="my-2 block w-full rounded-lg bg-dark-grayish-blue md:my-4"
+									className="my-2 block h-[46px]  w-80 rounded-lg bg-dark-grayish-blue px-4 py-2 md:my-4"
 									type="password"
 									value={password}
 									onChange={(e) => setPassword(e.target.value)}
 									required
 								/>
 							</label>
-							<div className="flex h-12 w-full justify-center">
+							<div className="flex h-12 w-96 items-center justify-center">
 								{submitted ? (
 									<div className="flex flex-row gap-2">
-										<div className="h-4 w-4 animate-bounce rounded-full bg-blue-700 [animation-delay:.7s]"></div>
-										<div className="h-4 w-4 animate-bounce rounded-full bg-blue-700 [animation-delay:.3s]"></div>
-										<div className="h-4 w-4 animate-bounce rounded-full bg-blue-700 [animation-delay:.7s]"></div>
+										<div className="h-4 w-4 animate-bounce rounded-full bg-animation-green [animation-delay:.3s]"></div>
+										<div className="h-4 w-4 animate-bounce rounded-full bg-animation-green [animation-delay:.7s]"></div>
+										<div className="h-4 w-4 animate-bounce rounded-full bg-animation-green [animation-delay:.7s]"></div>
 									</div>
 								) : (
 									<button
 										type="submit"
-										className="align-center w-full rounded-xl bg-midnight-blue p-1 font-normal md:p-2"
+										className=" h-[47px] w-72 rounded-xl border border-animation-green p-1 font-normal hover:text-animation-green md:p-2"
 									>
 										Sign In
 									</button>
@@ -150,30 +173,24 @@ const LoginPage = () => {
 						</form>
 					</div>
 				</div>
-				<div className="text-xs md:text-lg">
-					New User?
-					<NavLink to="/sign-up" className="text-blue-500">
+				<div className="text-xs font-bold md:text-lg">
+					New User?{" "}
+					<NavLink to="/sign-up" className="font-bold text-animation-green ">
 						Sign Up
 					</NavLink>
 				</div>
 				<div
-					className={`absolute top-0 z-10 flex h-32 w-52 origin-top flex-col items-center justify-center gap-1 rounded-3xl bg-dark-grayish-blue p-2 font-source-code-pro duration-200 md:w-96 md:p-4 ${
+					className={`absolute top-0 z-10 flex h-16 w-[30rem] border-spacing-2 origin-top flex-col items-center justify-center gap-1 rounded-3xl  bg-dark-grayish-blue p-2 font-source-code-pro tracking-widest duration-200  md:p-4 ${
 						!showPopUp
 							? "scale-y-0"
 							: msg["color"] === "green"
-							? "scale-y-150 border-8 border-green-600 text-green-600"
-							: "scale-y-150 border-8 border-red-600 text-red-600"
+							? "scale-y-150 border-b-2 border-l-2 border-r-2 border-green-600 text-green-600"
+							: "scale-y-150 border-b-2 border-l-2 border-r-2 border-red-600 text-red-600"
 					}`}
 				>
-					<span className="flex basis-3/4 items-center text-base font-semibold md:text-xl">
+					<span className="flex basis-3/4 items-center text-base  font-semibold md:text-xl">
 						{msg["msg"]}
 					</span>
-					<button
-						onClick={handlePopup}
-						className="duration-400 relative flex items-center self-end overflow-hidden rounded-[50px] bg-black px-8 py-1 text-xs font-bold uppercase text-white shadow-md transition-all ease-in-out before:absolute before:-left-full before:top-0 before:z-[-1] before:h-full before:w-full before:rounded-[50px] before:bg-gradient-to-r before:from-blue-500 before:to-blue-300 before:transition-all before:duration-500 before:ease-in-out hover:scale-105 hover:text-white hover:shadow-lg hover:before:left-0 active:scale-90 md:text-sm"
-					>
-						CLOSE
-					</button>
 				</div>
 			</div>
 		</>
--- src/_auth/forms/SignUp.tsx
+++ src/_auth/forms/SignUp.tsx
@@ -1,8 +1,12 @@
 import { useNavigate, NavLink, Navigate } from "react-router-dom";
 import clubLogo from "../../assets/images/club-logo.png";
+import eventLogo from "../../assets/images/passwordctf-logo.png";
+
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { useState } from "react";
 import { useUserContext } from "../../context/AuthContext";
+import { URL_ORIGIN } from "../../constants";
+import Background from "../../components/shared/Background";
 
 interface SignupResponse {
 	msg_code: string;
@@ -21,8 +25,12 @@ const SignUp = () => {
 	const msgCode = (status: number, msg_code: string) => {
 		if (status >= 200 && status < 400) {
 			setMsg({ msg: msg_code, color: "green" });
+			handlePopup();
+			setShowPopUp(true);
 		} else if (status >= 400 && status < 500) {
 			setMsg({ msg: msg_code, color: "red" });
+			handlePopup();
+			setShowPopUp(true);
 		}
 		console.log(msg);
 	};
@@ -32,7 +40,7 @@ const SignUp = () => {
 		setSubmitted(true);
 
 		axios
-			.post<SignupResponse>("http://localhost:5000/api/auth/signup", {
+			.post<SignupResponse>(`${URL_ORIGIN}/auth/signup`, {
 				name: name,
 				password: password,
 				members: members,
@@ -66,39 +74,56 @@ const SignUp = () => {
 	};
 
 	const handlePopup = () => {
-		setShowPopUp(false);
+		setShowPopUp(true);
+		setTimeout(() => {
+			setShowPopUp(false);
+			setSubmitted(false);
+		}, 1800);
 	};
 
 	console.log(submitted);
 	return (
 		<>
 			{isAuthenticated && <Navigate to="/sign-in"></Navigate>}
-			<div className="flex  flex-col items-center justify-center gap-2 p-20 font-source-code-pro">
-				<div className="box flex flex-col justify-center rounded-lg bg-black p-6 md:p-10">
-					<div className="flex w-full basis-1/4 flex-col gap-4 md:w-96 md:gap-6">
-						<div className="flex justify-center ">
+
+			<Background />
+			<div className="left-1/8 absolute top-[100px] z-10 flex h-auto w-[750px] flex-col items-center justify-center gap-2 overflow-auto p-11 font-source-code-pro">
+				<div className=" flex h-full w-full flex-col items-center justify-center rounded-lg border border-animation-green bg-black p-6 shadow-md shadow-animation-green md:p-10">
+					<div className="flex w-full basis-1/4 flex-col items-center justify-center gap-4 md:w-96 md:gap-6">
+						<div className="flex justify-center">
 							<div
-								className={`flex h-12 w-48 items-center justify-center text-lg font-bold md:h-16 md:w-60 md:text-xl`}
+								className={`flex h-12 w-48 items-center justify-center gap-4 text-lg font-bold md:h-16 md:w-60 md:text-xl`}
 							>
 								<img
 									src={clubLogo}
-									alt=""
-									className="h-full w-full object-contain"
+									alt="logo"
+									className="h-16 w-16 object-contain"
 								/>
-								<span>PasswordCTF</span>
+								<h2
+									className="hero glitch layers font-source-code-pro"
+									data-text="Linux Club"
+								>
+									<span className=" font-source-code-pro">PasswordCTF</span>
+								</h2>
+								<img
+									src={eventLogo}
+									alt="event-logo"
+									className="h-16 w-16 object-contain"
+								/>
 							</div>
 						</div>
-						<div className="text-center text-base font-semibold md:text-lg">
-							Create a new account
+						<div className="text-center text-[25px] font-bold  text-animation-green">
+							Hackers Required: <br />
+							Join the Space CTF
 						</div>
-						<div className="text-center text-sm  text-gray36 ">
-							To solve ctf, please enter your details
+						<div className="mt-11 text-center  text-sm text-gray-300">
+							Breach the Firewall, Enter the Nebula: Sign Up for the Cosmic
+							Challenge!
 						</div>
 					</div>
-					<div className="basis-3/4 md:w-96">
+					<div className="w-96 basis-3/4">
 						<form
-							id="sign-up-form"
-							className="flex flex-col items-center justify-center gap-2 p-6 text-base md:gap-4 md:p-10 md:text-lg"
+							className="flex w-full flex-col items-center justify-center gap-2 p-6 text-base md:gap-4 md:p-10 md:text-lg"
 							onSubmit={(e) => {
 								e.preventDefault();
 								handleSubmit(e);
@@ -107,7 +132,7 @@ const SignUp = () => {
 							<label className="w-full">
 								Team Name
 								<input
-									className="my-2 block w-full rounded-lg bg-dark-grayish-blue md:my-4"
+									className="my-2 block h-[46px] w-80 rounded-lg bg-dark-grayish-blue px-4 py-2 md:my-4"
 									type="text"
 									value={name}
 									onChange={(e) => setName(e.target.value)}
@@ -115,19 +140,9 @@ const SignUp = () => {
 								/>
 							</label>
 							<label className="w-full">
-								Password
-								<input
-									className="my-2 block w-full rounded-lg bg-dark-grayish-blue md:my-4"
-									type="password"
-									value={password}
-									onChange={(e) => setPassword(e.target.value)}
-									required
-								/>
-							</label>
-							<label className="w-full">
 								Member 1
 								<input
-									className="my-1 block w-full rounded-lg bg-dark-grayish-blue md:my-2"
+									className="my-1 block h-[46px]  w-80 rounded-lg bg-dark-grayish-blue px-4 py-2 md:my-2"
 									type="text"
 									value={members[0]}
 									onChange={(e) => handleInputChange(0, e.target.value)}
@@ -137,7 +152,7 @@ const SignUp = () => {
 							<label className="w-full">
 								Member 2
 								<input
-									className="my-2 block w-full rounded-lg bg-dark-grayish-blue md:my-4"
+									className="my-1 block h-[46px]  w-80 rounded-lg bg-dark-grayish-blue px-4 py-2 md:my-2"
 									type="text"
 									value={members[1]}
 									onChange={(e) => handleInputChange(1, e.target.value)}
@@ -146,55 +161,59 @@ const SignUp = () => {
 							<label className="w-full">
 								Member 3
 								<input
-									className="my-2 block w-full rounded-lg bg-dark-grayish-blue md:my-4"
+									className="my-1 block h-[46px]  w-80 rounded-lg bg-dark-grayish-blue px-4 py-2 md:my-2"
 									type="text"
 									value={members[2]}
 									onChange={(e) => handleInputChange(2, e.target.value)}
 								/>
 							</label>
-							<div className="flex h-12 w-full justify-center">
+							<label className="w-full">
+								Password
+								<input
+									className="my-2 block h-[46px]  w-80 rounded-lg bg-dark-grayish-blue px-4 py-2 md:my-4"
+									type="password"
+									value={password}
+									onChange={(e) => setPassword(e.target.value)}
+									required
+								/>
+							</label>
+							<div className="flex h-12 w-96 items-center justify-center">
 								{submitted ? (
 									<div className="flex flex-row gap-2">
-										<div className="h-4 w-4 animate-bounce rounded-full bg-blue-700 [animation-delay:.7s]"></div>
-										<div className="h-4 w-4 animate-bounce rounded-full bg-blue-700 [animation-delay:.3s]"></div>
-										<div className="h-4 w-4 animate-bounce rounded-full bg-blue-700 [animation-delay:.7s]"></div>
+										<div className="h-4 w-4 animate-bounce rounded-full bg-animation-green [animation-delay:.7s]"></div>
+										<div className="h-4 w-4 animate-bounce rounded-full bg-animation-green [animation-delay:.3s]"></div>
+										<div className="h-4 w-4 animate-bounce rounded-full bg-animation-green [animation-delay:.7s]"></div>
 									</div>
 								) : (
 									<button
 										type="submit"
-										className="align-center w-full rounded-xl bg-midnight-blue p-1 font-normal md:p-2"
+										className=" h-[47px] w-72 rounded-xl border border-animation-green p-1 font-normal hover:text-animation-green md:p-2"
 									>
-										Sign In
+										Sign Up
 									</button>
 								)}
 							</div>
 						</form>
 					</div>
 				</div>
-				<div className="text-xs md:text-lg">
-					Already Logged In?{" "}
-					<NavLink to="/sign-in" className="text-blue-500">
-						Log In
+				<div className="text-xs font-bold md:text-lg">
+					Already have an account?
+					<NavLink to="/sign-in" className="font-bold text-animation-green">
+						Sign In
 					</NavLink>
 				</div>
 				<div
-					className={`absolute top-0 z-10 flex h-32 w-52 origin-top flex-col items-center justify-center gap-1 rounded-3xl bg-dark-grayish-blue p-2 font-source-code-pro duration-200 md:w-96 md:p-2 ${
+					className={`absolute top-0 z-10 flex h-16 w-[30rem] border-spacing-2 origin-top flex-col items-center justify-center gap-1 rounded-3xl  bg-dark-grayish-blue p-2 font-source-code-pro tracking-widest duration-200  md:p-4 ${
 						!showPopUp
 							? "scale-y-0"
 							: msg["color"] === "green"
-							? "scale-y-150 border-8 border-green-600 text-green-600"
-							: "scale-y-150 border-8 border-red-600 text-red-600"
+							? "scale-y-150 border-b-2 border-l-2 border-r-2 border-green-600 text-green-600"
+							: "scale-y-150 border-b-2 border-l-2 border-r-2 border-red-600 text-red-600"
 					}`}
 				>
-					<span className="flex basis-3/4 items-center text-base font-semibold md:text-xl">
+					<span className="flex basis-3/4 items-center text-base  font-semibold md:text-xl">
 						{msg["msg"]}
 					</span>
-					<button
-						onClick={handlePopup}
-						className="duration-400 relative flex items-center self-end overflow-hidden rounded-[50px] bg-black px-8 py-1 text-xs font-bold uppercase text-white shadow-md transition-all ease-in-out before:absolute before:-left-full before:top-0 before:z-[-1] before:h-full before:w-full before:rounded-[50px] before:bg-gradient-to-r before:from-blue-500 before:to-blue-300 before:transition-all before:duration-500 before:ease-in-out hover:scale-105 hover:text-white hover:shadow-lg hover:before:left-0 active:scale-90 md:text-sm"
-					>
-						CLOSE
-					</button>
 				</div>
 			</div>
 		</>
