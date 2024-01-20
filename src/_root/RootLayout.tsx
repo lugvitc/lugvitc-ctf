00939f687fb1e400b6677faa34427e99a65ecd6a
Shreyas <shreyas.mohanty03@gmail.com>
2024-01-21 01:03:00 +0530

    navbar before 29 feature removed

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -3,24 +3,12 @@ import Navbar from "../components/shared/Navbar";
 import AuthProvider from "../context/AuthContext";
 
 const RootLayout = () => {
-	const currentDate = new Date();
-	const targetDate = new Date("2024-01-29");
-
-	const renderAuthProviderAndNavbar = () => {
-		if (currentDate > targetDate) {
-			return (
-				<AuthProvider>
-					<Navbar />
-				</AuthProvider>
-			);
-		}
-		return null;
-	};
-
 	return (
 		<>
 			<div className="w-full md:flex">
-				{renderAuthProviderAndNavbar()}
+				<AuthProvider>
+					<Navbar />
+				</AuthProvider>
 
 				<section className="flex w-full items-center justify-center">
 					<Outlet />
